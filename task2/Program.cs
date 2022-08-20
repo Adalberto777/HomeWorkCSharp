f427ad917d88089ba0533969c0d6be7015361435
Adalberto777 <albert.tliashev@gmail.com>
2022-08-20 20:09:02 +0500

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter a first integer");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter a second integer");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter a thid integer");
+int number3 = int.Parse(Console.ReadLine());
+int max = number1;
+
+if(max > number2)  max = number2;
+if(max > number3)  max = number3;
+
+Console.Write($"из трех чисел {number1}, {number2} и {number3} большее {max}");
