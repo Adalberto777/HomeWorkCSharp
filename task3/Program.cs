c512224148286185fe86a436de056398889372e9
Adalberto777 <albert.tliashev@gmail.com>
2022-08-20 20:21:28 +0500

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Enter an integer");
+int number1 = int.Parse(Console.ReadLine());
+
+if(number1 % 2 ==0) Console.Write($" {number1} - четное число");
+else Console.Write($" {number1} - не четное число");
