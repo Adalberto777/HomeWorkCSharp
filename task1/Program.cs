c1bf8e5bd12ba6e13e54bdffeb2a503b840c7a06
Adalberto777 <albert.tliashev@gmail.com>
2022-08-20 19:56:39 +0500

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter a first integer");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter a second integer");
+int number2 = int.Parse(Console.ReadLine());
+int max = 0;
+
+if(number1 > number2)  max = number1;
+if(number1 < number2)  max = number2;
+
+Console.Write($"из двух чисел {number1} и {number2} большее {max}");
