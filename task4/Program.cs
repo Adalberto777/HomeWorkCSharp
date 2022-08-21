e656d3d4b81ff04893a975483242699d95d43bc0
Adalberto777 <albert.tliashev@gmail.com>
2022-08-21 13:06:46 +0500

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter an integer");
+int number = int.Parse(Console.ReadLine());
+int count = 2;
+if(number < 2) Console.Write($"в {number} нет четных чисел");
+while(count <= number)
+{
+    Console.Write($"{count}, ");
+    count = count + 2;
+}
+
+ 
